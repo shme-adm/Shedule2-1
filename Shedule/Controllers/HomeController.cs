8044faf7434c0e2cb61ecfac55b4f35ddfcc4bea
Администратор <@DESKTOP-M0KENQP>
2018-04-28 15:31:28 +0400

    Добавьте файлы проекта.

---
+++ Shedule/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shedule.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Shedule()
+        {
+            ViewBag.Message = "Здесь будет расписание";
+
+            return View();
+        }
+        public ActionResult Reports()
+        {
+            ViewBag.Message = "Здесь будут отчеты";
+
+            return View();
+        }
+    }
+}
---
+++ Shedule/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shedule.MvcApplication" Language="C#" %>
